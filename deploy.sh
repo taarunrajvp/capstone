6c93983c00fe732f92f0bf6d87e9d99def321930
TAARUNRAJ V P <taarunrajvp@gmail.com>
2025-03-21 15:31:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     # echo hi1234
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u taarunraj -p dckr_pat_CA72jfw0MYZo8kKVfRtsKzNHG-g
+   docker login -u taarunraj -p dckr_pat_lxmJ1AsEt6I3QULgxzDRr7h-w-w
     docker tag test taarunraj/dev
     docker push taarunraj/dev
     
